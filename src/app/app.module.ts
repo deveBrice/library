f4d7fb92fde98e144bdfb97a00b78bd232a4df3f
deveBrice <brice.william971@gmail.com>
2021-11-30 16:07:02 +0100

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { Routes, RouterModule } from '@angular/router';
+
+const Router: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'articleList', component: HomeComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+]
 
 @NgModule({
   declarations: [
@@ -12,9 +24,15 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    RouterModule.forRoot(Router)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
